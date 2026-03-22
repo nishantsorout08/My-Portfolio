2c665c471a492cde9eea67efc4b91a16dc75dcc0
nishantsorout08 <nishantsorout07@gmail.com>
2026-03-22 19:12:27 +0530

    automated with github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/My-Portfolio/" : "/",
-});
+  base: mode === "production" ? "/My-Portfolio/" : "/",
+}));
